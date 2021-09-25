ea64925b6a075f1bd1f4f3ec19fb473afd73b33e
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-09-25 14:29:17 +0300

    Task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,8 +15,25 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  if(isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  }
+  if(isNaN(contribution)) {
+     return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  }
+  if(isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+  if(isNaN(date)) {
+    return totalAmount = `Параметр "Дата" содержит неправильное значение "${date}"`;
+  }
 
-  // код для задачи №2 писать здесь
+  let creditBody = amount - contribution;
+  let month = date.getUTCMonth() - new Date().getUTCMonth() + (date.getFullYear() - new Date().getFullYear()) * 12;
+  let paymentForMonth = creditBody * (percent / 12 / 100 + (percent / 12 / 100) / (Math.pow((1 + percent / 12 / 100), month)) - 1));
+
+  totalAmount = Number((paymentForMonth * month).toFixed(2));
+  console.log(totalAmount);
 
   return totalAmount;
 }
