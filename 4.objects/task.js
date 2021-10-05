d67eff8e2f095d184640c960a51664a6bae98f4d
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-10-05 09:50:49 +0300

    Task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){
+  if (this.marks === undefined){ 
+    this.marks = [];
+    this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark){
+  if (this.marks === undefined) this.marks = [];
+  for (element of mark) this.marks.push(element);
+}
+
+Student.prototype.getAverage = function (){
+  let sum = 0;
+  for (element of this.marks) sum += element;
+  return (sum / this.marks.length);
+}
+
+Student.prototype.exclude = function (reason){
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
