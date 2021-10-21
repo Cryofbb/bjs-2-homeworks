cb277b9a2b13d74e73620c1b4e3b0af0c9186fba
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-10-21 17:10:42 +0300

    Task 7

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,18 @@
-// тут вы можете вызывать функции из task.js
+function testCase() {
+    clock = new AlarmClock();
+    clock.addClock("10:01", () => console.log(`Add clock`), 1);
+    clock.addClock("10:02", () => {console.log(`Add clock and then remove`); clock.removeClock(2)}, 2);
+
+    clock.addClock("10:03", () => {
+        console.log(`stop+clear+check alarms`);
+        clock.stop();
+        clock.clearAlarms();
+        clock.printAlarms();
+    }, 3);
+
+    clock.printAlarms();
+
+    clock.start();
+}
+
+testCase();
