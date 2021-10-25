a3204641eb62df3669df9f7358263ef5dba1daa5
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-10-25 12:26:54 +0300

    Task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,38 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  function wrapper(...args) {
+    let hash = args.join(',');
+    if (hash in cache) {
+      return "Из кэша: " + cache[hash];
+    } else {
+      cache[hash] = func.apply(this, args);
+      if (Object.keys(cache).length > 4) delete cache[Object.keys(cache)[0]];
+      return "Вычисляем: " + cache[hash];
+    }
+  }
+  return wrapper;
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let flag;
+  function wrapper (...args) {
+    if (flag) return;
+    func.apply(this, ...args);
+    flag = true;
+    setTimeout(() => {flag = false}, ms);
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let flag;
+  function wrapper (...args) {
+    wrapper.history++;
+    if (flag) return;
+    func.apply(this, ...args);
+    flag = true;
+    setTimeout(() => {flag = false}, ms);
+  }
+  wrapper.history = 0;
+  return wrapper;
 }
